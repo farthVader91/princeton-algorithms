2ba4f54cf17aacbc9a74d83be53ee9599ff503c7
Vishal Gowda <cartmanboy1991@gmail.com>
2017-01-26 15:58:59 +0530

    Add all input elements into RandomizedQueue

--- week2/Permutation.java
+++ week2/Permutation.java
@@ -5,16 +5,7 @@ public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> rq = new RandomizedQueue<String>();
         int k = Integer.parseInt(args[0]);
-        String[] input = StdIn.readAllStrings();
-        for (int i = 0; i < k; i++) {
-            rq.enqueue(input[i]);
-        }
-        while (true) {
-            try {
-                System.out.println(rq.dequeue());
-            } catch (NoSuchElementException nse) {
-                break;
-            }
-        }
+        while(!StdIn.isEmpty()) rq.enqueue(StdIn.readString());
+        for(int i = 0; i < k; ++i) System.out.println(rq.dequeue());
     }
 }
